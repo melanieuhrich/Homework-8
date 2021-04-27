2ac349a956b2312aa03ad863263380a316f90434
mel <muhrich6@gmail.com>
2021-04-27 18:21:51 -0500

    almost done

--- Main/generateHtml.js
+++ Main/generateHtml.js
@@ -7,7 +7,6 @@ function generateHtml(cardHTML) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link rel="stylesheet" href="style.css">
         <title>Team Generator</title>
     </head>
     
--- Main/index.js
+++ Main/index.js
@@ -1,15 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHtml = require('./generateHtml')
-const Employee = require('./lib/employee');
+const generateHtml = require('./generateHtml');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const { WSAEHOSTDOWN } = require('constants');
 
 const employees = [];
-
-// let role = Employee.getRole()
 
 const managerQuestions = [
     {
@@ -34,49 +30,49 @@ const managerQuestions = [
     }
 ]
 
-const internQuestions = [
-    {
-        type: 'input',
-        message: "What is the intern's name?",
-        name: 'iName'
-    },
-    {
-        type: 'input',
-        message: "What is the intern's id?",
-        name: 'iId'
-    },
-    {
-        type: 'input',
-        message: "What is the intern's email?",
-        name: 'iEmail'
-    },
-    {
-        type: 'input',
-        message: "What is the intern's school?",
-        name: 'iSchool'
-    },
-]
-
 const engineerQuestions = [
     {
         type: 'input',
         message: "What is the engineer's name?",
-        name: 'eName'
+        name: 'name'
     },
     {
         type: 'input',
         message: "What is the engineer's id?",
-        name: 'eId'
+        name: 'id'
     },
     {
         type: 'input',
         message: "What is the engineer's email?",
-        name: 'eEmail'
+        name: 'email'
     },
     {
         type: 'input',
         message: "What is the engineer's GitHub username?",
-        name: 'eGithub'
+        name: 'github'
+    },
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "What is the intern's name?",
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's id?",
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's email?",
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's school?",
+        name: 'school'
     },
 ]
 
@@ -89,46 +85,61 @@ const memberQuestion = {
 
 function init() {
     inquirer
-        .prompt(managerQuestions)  //prompt the user for responses
-        .then(data => {   //THEN AFTER the user have given us responses
-            console.log(data);
+        .prompt(managerQuestions) 
+        .then(data => {   
             const manager = new Manager(data.name, data.id, data.email, data.office);
             employees.push(manager);
             memberSelect();
         })
 }
 
-/**
- * 
- * employees = [{manager obj}, {engi1}, {engi2}, {int}]
- * {
- * name: "Bob Jones",
- * id: "3",
- * email: "bobjones@gmail.com",
- * office: "4B",
- * role: "Manager"
- * }
- * function templateMaker (dataAboutOneEmployee) {
- * return 
- * var template = `
- * div.card
- *  h1 ${dataABoutOneEmployee.name}
- *  h3 ${role}`
- * 
- * }
- * employees.forEach(employee=> {
- * templateMaker(employee)})
- */
-function miscProperty(employee) {
-    if (employee.getRole() == 'Engineer') {
-        return `<p>GitHub: <a href='[${employee.eGithub}](https://github.com/${employee.eGithub})'></a></p>`
-    } else if (employee.getRole() == 'Manager') {
-        return `<p>Office Number: ${employee.getOfficeNumber()}</p>`
-    } else {
-        return `<p>School: ${employee.iSchool}</p>`
-    }
-
+function memberSelect() {
+    inquirer
+        .prompt(memberQuestion)
+        .then(data => {
+            if (data.member == 'Engineer') {
+                inputEngineer()
+            } else if (data.member == 'Intern') {
+                inputIntern()
+            } else {
+                writeFile(generateHtml(appendCards()))
+            }
+        })
 }
+
+function inputEngineer() {
+    inquirer
+        .prompt(engineerQuestions)
+        .then(data => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+            employees.push(engineer);
+            memberSelect()
+        })
+}
+
+function inputIntern() {
+    inquirer
+        .prompt(internQuestions)
+        .then(data => {
+            const intern = new Intern(data.name, data.id, data.email, data.school);
+            employees.push(intern);
+            memberSelect()
+        })
+}
+
+function writeFile(htmlString) {
+    fs.writeFile('./dist/index.html', htmlString, (error) =>
+        error ? console.error(error) : console.log('Generating HTML...')) 
+}
+
+function appendCards() {
+    let html = ''; 
+    employees.forEach(employee => {
+        html += getInfo(employee)
+    });
+    return html;
+}
+
 function getInfo(employee) { 
     let template = 
     `<div class="col s12 m6">
@@ -139,7 +150,7 @@ function getInfo(employee) {
                 <hr>
                 <p>ID: ${employee.id}</p>
                 <hr>
-                <p>Email: <a href="#">${employee.email}</a></p>
+                <p>Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
                 <hr>
                 ${miscProperty(employee)} 
             </div>
@@ -148,81 +159,14 @@ function getInfo(employee) {
     return template;
 }
 
-function appendCards() {
-    //there are no cards here
-    let html = ''; 
-
-    // console.log('working')
-    employees.forEach(employee => {
-        html += getInfo(employee)
-    });
-    // getInfo();
-    // htnml variable is a whole bunch of html cards for each employee as a string
-    // fs.writeFile('./dist/index.html', html, (error) =>
-    // error ? console.error(error) : console.log(''))
-    return html;
-}
-
-function memberSelect() {
-    // prompt user about what type of employee they are adding
-    inquirer.prompt(memberQuestion)
-        .then(data => {
-            if (data.member == 'Engineer') {
-                inputEngineer()
-            } else if (data.member == 'Intern') {
-                inputIntern()
-            } else {
-                writeFile(generateHtml(appendCards()))
-                // .then(appendCards())
-            }
-        })
-}
-
-function inputEngineer() {
-    inquirer
-        .prompt(engineerQuestions)
-        .then(data => {
-            console.log(data);
-            const engineer = new Engineer(data.eName, data.eId, data.eEmail, data.eGithub);
-            employees.push(engineer);
-            memberSelect()
-        })
-}
-
-function inputIntern() {
-    inquirer
-        .prompt(internQuestions)
-        .then(data => {
-            console.log(data);
-            const intern = new Intern(data.iName, data.iId, data.iEmail, data.iSchool);
-            employees.push(intern);
-            memberSelect()
-        })
-}
-
-// the story of a user using this cla
-
-/**
- * 
- user starts the app -  node index.js
- user gets prompted with questions regarding the manager  --
- answers from user are added to employee array ---
- then user is prompted is next employee member intern engineer or no more ---
-if theres engineer
-    do engineer stuff
-if intern
-    do intern stuff
-
-    --> both these lead to asking if theres another employee 
-
-if no more
-    take all the data from employee array and generate the html
- */
-
-function writeFile(htmlString) {
-    fs.writeFile('./dist/index.html', htmlString, (error) =>
-        error ? console.error(error) : console.log('Generating HTML...')) 
-        // .then(appendCards(employeeData))
+function miscProperty(employee) {
+    if (employee.getRole() == 'Engineer') {
+        return `<p>GitHub: <a href="https://github.com/${employee.github}" target="_blank"c>${employee.github}</a></p>`
+    } else if (employee.getRole() == 'Manager') {
+        return `<p>Office Number: ${employee.officeNumber}</p>`
+    } else {
+        return `<p>School: ${employee.school}</p>`
+    }
 }
 
 init();
